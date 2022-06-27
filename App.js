3b85288135870e865031a707d6d615cceb1b5780
medicharla2 <98125181+medicharla2@users.noreply.github.com>
2022-06-26 21:03:27 -0400

    added mapView with example markers and user location

--- App.js
+++ App.js
@@ -1,13 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import MapView, { Marker } from 'react-native-maps';
 
-export default function App() {
-  return (
+let signedIn = true;
+
+function HomeScreen(){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>home</Text>
+
     </View>
   );
+}
+
+function NearYou(){
+  const loc = {
+    latitude: 41.739,
+    longitude: -87.554,
+  }
+
+  return(
+      <View style={styles.container}>
+        <MapView style={styles.map} showsUserLocation={true}>
+          <Marker coordinate = {loc}></Marker>
+        </MapView>
+      </View>
+  );
+}
+
+function SignIn(){
+  return(
+    <View style={styles.container}>
+      <Text>SignIn</Text>
+    </View>
+  );
+}
+
+function NewAcct(){
+  return(
+    <View style={styles.container}>
+      <Text>acct</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  if(signedIn){
+    console.log(signedIn);
+    return(
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
+          <Tab.Screen name="NearYou" component={NearYou}></Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }else{
+    console.log(signedIn);
+    return(
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="SignIn" component={SignIn}></Tab.Screen>
+          <Tab.Screen name="NewAcct" component={NewAcct}></Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +78,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
